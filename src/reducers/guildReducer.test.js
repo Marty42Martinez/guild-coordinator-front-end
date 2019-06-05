1244d84ce1b66c552f9b7e43c12ca91e7b1a05a1
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-05 15:16:11 -0700

    first reducer test failing

--- src/reducers/guildReducer.test.js
+++ src/reducers/guildReducer.test.js
@@ -1,2 +1,25 @@
 import { FETCH_GUILD_LIST, FETCH_GUILD_LIST_PENDING, FETCH_GUILD_LIST_REJECTED } from '../actions/guilds/guildActions';
+import reducer from './guildReducer';
 
+describe('Guild Reducer tests', () => {
+  it('handles the FETCH action', () => {
+    const initialState = {
+      list: [],
+      loading: false,
+      error: null
+    };
+    const action = {
+      type: FETCH_GUILD_LIST,
+      payload: ['Apocalypse', 'IgnisRising']
+    };
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({
+      list: ['Apocalypse', 'IgnisRising'],
+      loading: false,
+      error: null
+    });
+
+
+  });
+});
