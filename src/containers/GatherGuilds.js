b622e029ac6ae95e1b986af9a08855ea381230cf
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-05 16:14:52 -0700

    fill out Gather container with selectors

--- src/containers/GatherGuilds.js
+++ src/containers/GatherGuilds.js
@@ -1,32 +1,46 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getGuildList, getGuildLoading, getGuildError } from '../selectors/guildSelectors';
+import { fetchGuildList } from '../actions/guilds/guildActions';
+import GuildList from '../components/guilds/GuildList';
+
 
 class GatherGuilds extends PureComponent {
   static propTypes = {
-
+    fetch: PropTypes.func.isRequired,
+    list: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.object
   }
 
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+
   render() {
+    const { list, loading } = this.props;
+
+    if(loading) return <h1>Loading!</h1>;
 
     return (
       <section>
-        <h2>Words and more Words</h2>
+        <GuildList guilds={list} />
       </section>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  //selectors!
-  list: [],
-  loading: '',
-  error: ''
+  list: getGuildList(state),
+  loading: getGuildLoading(state),
+  error: getGuildError
 });
 
 const mapDispatchToProps = dispatch => ({
   fetch() {
-    dispatch(fetchGuilds());
+    dispatch(fetchGuildList());
   }
 });
 
