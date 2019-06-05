a49a49714ad76729504d7ca9b6c12d2d3dc658bb
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-05 16:23:50 -0700

    Guild Form shell set up

---
+++ src/components/guilds/GuildForm.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class GuildForm extends PureComponent {
+  static propTypes = {
+
+  }
+
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <form>
+
+      </form>
+    );
+  }
+}
