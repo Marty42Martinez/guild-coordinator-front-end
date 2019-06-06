ad5b0313b05f3fc6b98713fa5915f1719346f07b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-05 17:04:34 -0700

    Guild Form populated

--- src/components/guilds/GuildForm.js
+++ src/components/guilds/GuildForm.js
@@ -3,18 +3,48 @@ import PropTypes from 'prop-types';
 
 export default class GuildForm extends PureComponent {
   static propTypes = {
-
+    onSubmit: PropTypes.func.isRequired
   }
 
   state = {
+    name: '',
+    image: '',
+    game: ''
+  }
 
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { name, image, game } = this.state;
+    this.props.onSubmit(name, image, game);
+    this.setState({ name: '', image: '', game: '' });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
-
+    const { name, image } = this.state;
     return (
-      <form>
-
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Guild Name
+          <input type="text" name="name" value={name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Guild Image URL
+          <input type="text" name="image" value={image} onChange={this.handleChange} />
+        </label>
+        <label>
+          Choose a Game
+          <select name="game" onChange={this.handleChange}>
+            <option value="" selected>--Please select an option--</option>
+            <option value="WorldOfWarcraft">World of Warcraft</option>
+            <option value="DungeonsAndDragons">Dungeons &amp; Dragons</option>
+          </select>
+        </label>
+        <button>Form a Guild</button>
       </form>
     );
   }
