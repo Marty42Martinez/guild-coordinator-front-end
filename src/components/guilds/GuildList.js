17abc18d91c5c5bb0e0b508e9eb79c59ef98b424
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-06 15:55:44 -0700

    Moved logic 2 seperate component due to repitition

--- src/components/guilds/GuildList.js
+++ src/components/guilds/GuildList.js
@@ -1,59 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GuildLink from './GuildLink';
+import GuildListByGame from './GuildListByGame';
 
 const guildsByGame = guilds => {
-  // if(guilds.length === 0) return [];
   return [...guilds].reduce(function(acc, current) {
     if(!acc[current['game']]) { acc[current['game']] = []; }
     acc[current['game']].push({ ...current });
     return acc;
   }, {});
-  
-  // const gameObj = {
-  //   WorldOfWarcraft: [],
-  //   DungeonsAndDragons: []
-  // };
-
-  // guilds.forEach(guild => {
-  //   gameObj[guild.game] = [...gameObj[guild.game], guild];
-  // });
-
-  // return gameObj;
 };
 
 function GuildList({ guilds }) {
   const gameObj = guildsByGame(guilds);
-  console.log(gameObj);
 
-  const games = Object.keys(gameObj).map(game => {
-    const gameList = gameObj[game].map(guild => (
-      <li key={guild._id}>
-        <GuildLink guild={guild} />
-      </li>
-    ));
-    return (
-      <>
-        <h2>{game}</h2>
-        <ul>
-          {gameList}
-        </ul>
-      </>
-    );
-  });
-  // const wowList = gameObj.WorldOfWarcraft.map(guild => (
-  //   <li key={guild._id}>
-  //     <GuildLink guild={guild} />
-  //   </li>
-  // ));
-
-  // const dndList = gameObj.DungeonsAndDragons.map(guild => (
-  //   <li key={guild._id}>
-  //     <GuildLink guild={guild} />
-  //   </li>
-  // ));
-  const wowList = [];
-  const dndList = [];
+  const games = Object.keys(gameObj).map((game, i) => (
+    <GuildListByGame key={i} game={game} list={gameObj[game]} />
+  ));
+  
   return (
     <section>
       {games}
---
+++ src/components/guilds/GuildListByGame.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import GuildLink from './GuildLink';
+
+function GuildListByGame({ game, list }) {
+  const gameList = list.map(guild => (
+    <li key={guild._id}>
+      <GuildLink guild={guild} />
+    </li>
+  ));
+  return (
+    <>
+      <h2>{game}</h2>
+      <ul>
+        {gameList}
+      </ul>
+    </>
+  );
+}
+
+GuildListByGame.propTypes = {
+  game: PropTypes.string.isRequired,
+  list: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    game: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  })).isRequired
+};
+
+export default GuildListByGame;
