fe98b429c1d0c60a8ed0722d1b5646e1685ee6e5
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-06 17:38:10 -0700

    restructure get calls

--- src/services/guildCoordinatorApi.js
+++ src/services/guildCoordinatorApi.js
@@ -2,15 +2,14 @@ import defaultImage from '../assets/honey-bear.png';
 import { get, post } from './restMethods';
 
 export const getAllGuilds = () => {
-  return get('some.ec2.server.compute.aws')
-    .then(json => ({
-      list: json.map(guild => ({
-        _id: guild._id,
-        name: guild.name,
-        image: guild.image || defaultImage,
-        game: guild.game
-      }))
-    }));
+  return get('http://18.217.24.159/api/v1/guilds/')
+    .then(json => (json.map(guild => ({
+      _id: guild._id,
+      name: guild.name,
+      image: guild.image || defaultImage,
+      game: guild.game
+    })
+    )));
 };
 
 export const postGuildToDB = (body) => {
