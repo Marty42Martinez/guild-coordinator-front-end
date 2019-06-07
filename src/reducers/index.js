843b934f1d4341bc2db5b1f9c405efd19b95e694
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-07 14:37:36 -0700

    combine reducer with events

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import guilds from './guildReducer';
+import events from './eventReducer';
 
 export default combineReducers({
-  guilds
+  guilds,
+  events
 });
