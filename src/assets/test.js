159540545454cd946d21b8b4541ded13389d75b0
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-07 15:01:10 -0700

    Hooked up Router to GatherEvents!

---
+++ src/assets/test.js
@@ -0,0 +1,66 @@
+[
+  {
+    '_id': '5cf9a7675c7d1d24206b45e0',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'Something Awesome',
+    'game': 'WoW'
+  },
+  {
+    '_id': '5cf9aec93640ff2459901742',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'MwA',
+    'game': 'DnD'
+  },
+  {
+    '_id': '5cfac9881c4af7081b1bc2f8',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'bad guild',
+    'game': 'World Of Warcraft'
+  },
+  {
+    '_id': '5cfac9ef1c4af7081b1bc2f9',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'better guild',
+    'game': 'Dungeons And Dragons'
+  },
+  {
+    '_id': '5cfaca491c4af7081b1bc2fa',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'Rock Lobster',
+    'game': 'World Of Warcraft'
+  },
+  {
+    '_id': '5cfacaea1c4af7081b1bc2fb',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'The Honey Bears',
+    'game': 'Dungeons And Dragons'
+  },
+  {
+    '_id': '5cfacbbe1c4af7081b1bc2fc',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'Craiiiig',
+    'game': 'Dungeons And Dragons'
+  },
+  {
+    '_id': '5cfacbf81c4af7081b1bc2fd',
+    'members': [
+      '5cf9a7095c7d1d24206b45df'
+    ],
+    'name': 'Smells Bad',
+    'game': 'World Of Warcraft'
+  }
+];
--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import {
   Route
 } from 'react-router-dom';
 import GatherGuilds from '../containers/GatherGuilds';
+import GatherEvents from '../containers/events/GatherEvents';
 
 export default function App() {
   return (
     <Router>
       <h1>Hello Dev!</h1>
       <Switch>
+        <Route exact path="/:id" component={GatherEvents} />
         <Route exact path="/" component={GatherGuilds} />
       </Switch>
     </Router>
